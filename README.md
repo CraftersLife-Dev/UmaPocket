b82fec99e5e8282234be16e8e611e61961449af1
SUDATI/eR <165920415+sudatti624@users.noreply.github.com>
2025-12-07 13:16:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Paper Plugin Template
+CraftersLifeの未来ある若者のために作った高機能テンプレート
+
+右上の [use this template\] を押せばテンプレートを使用可能
+
+## Gradle プラグイン
+
+### [indra licenser spotless](https://github.com/KyoriPowered/indra/wiki/indra-licenser-spotless)
+
+- **概要**: 全クラスのソースコードに一括でライセンスヘッダーを挿入
+- **メリット**: IntelliJ IDEAなどのIDEで個別に設定する手間を省き、開発環境が変わっても一貫したライセンス管理が可能
+  - 押したら俺のライセンスが全部消えるというすぐれもの！gitに残るから別にいいよ
+
+### [gremlin](https://github.com/jpenilla/gremlin)
+
+- **概要**: Paperプラグイン実行時に必要な依存関係を解決する
+- **メリット**: [PluginLoader](https://docs.papermc.io/paper/dev/getting-started/paper-plugins#loaders) の仕組みを利用してプラグインの依存関係を動的に解決可能
+
+### [Resource Factory](https://github.com/jpenilla/resource-factory)
+
+- **概要**: `paper-plugin.yml` をビルドスクリプトで定義
+- **メリット**: 設定ミスの削減と管理の簡略化
+
+### [run-task](https://github.com/jpenilla/run-task)
+
+- **概要**: IDE上で Paperサーバーを実行
+- **メリット**:
+  - サーバー起動だけでプラグインのテストが可能
+  - 依存プラグインを自動インストール
+  - ホットスワップでサーバー起動中にコードの変更が可能
+
+### [check-style](https://docs.gradle.org/current/userguide/checkstyle_plugin.html)
+
+- **概要**: コード品質を一定水準に保つ
+- **メリット**: 設計上の問題検出、コードレイアウトやフォーマットがコーディングスタイルに準拠しているかのチェックが可能
+  - 波括弧のインデントを揃える人間はストレスで病む
+
+## Java ライブラリ
+
+### [Paper API](https://docs.papermc.io/paper/dev/getting-started/paper-plugins)
+
+- **概要**: 説明不要
+- **メリット**:
+  - サーバー起動前に設定やデータベースなどのリソースを初期化可能
+    - Bukkitの一部API（例: `Server` や `ItemStack`）は、 `JavaPlugin#onEnable` 以降での呼び出しが必須であることに注意が必要
+    - JavaPluginの具象クラスの初期化もコントロール可能
+  - Mojangの[Brigadier](https://github.com/Mojang/brigadier)を基にしたコマンドの登録
+  - レジストリの編集（現状はエンチャントのみ対応）
+  - 実行時に必要なランタイムを自動ダウンロード
+  - カラーコードを駆逐せよ
+
+### [Configurate](https://github.com/SpongePowered/Configurate?tab=readme-ov-file)
+
+- **概要**: SpongePoweredが開発した設定ライブラリ
+- **メリット**:
+  - Javaオブジェクトと設定ファイルの相互変換
+  - カスタムType Serializerによる文字列変換
+
+### [MiniPlaceholders](https://github.com/MiniPlaceholders/MiniPlaceholders)
+
+- **概要**: MiniMessageのプレースホルダーを拡張するライブラリ 
+- **メリット**:
+  - 他プラグインが提供するタグを利用可能
+  - 他プラグインに提供するタグを登録可能
+  - [PlaceholderAPI拡張](https://modrinth.com/plugin/miniplaceholders-placeholderapi-expansion)もあるよ
+
+### [Kotonoha](https://github.com/NamiUni/kotonoha)
+
+- **概要**: メッセージをDRYに管理できるライブラリ
+- **メリット**:
+    - インターフェースを定義するだけでリソースバンドルを生成可能
+    - インターフェースのプロキシインスタンスを生成可能
+
---
+++ build.gradle.kts
@@ -0,0 +1,110 @@
+import xyz.jpenilla.resourcefactory.bukkit.Permission
+import xyz.jpenilla.resourcefactory.paper.PaperPluginYaml.Load
+
+val projectVersion: String by project
+version = projectVersion
+
+plugins {
+    id("java")
+    id("checkstyle")
+    alias(libs.plugins.shadow)
+    alias(libs.plugins.resource.factory)
+    alias(libs.plugins.run.paper)
+    alias(libs.plugins.gremlin)
+    alias(libs.plugins.indra.licenser.spotless)
+}
+
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+dependencies {
+    // Paper
+    compileOnly(libs.paper.api)
+
+    // Integrations
+    compileOnly(libs.mini.placeholders)
+
+    // Libraries
+    compileOnly(libs.configurate.yaml) // Paperに組み込んである
+    runtimeDownload(libs.adventure.serializer.configurate)
+    implementation(libs.kotonoha.message)
+    implementation(libs.kotonoha.message.extra.miniplaceholders)
+    implementation(libs.kotonoha.translator)
+}
+
+val mainPackage = "io.github.crafterslife.dev.papertemplate" // TODO: パッケージ名を変更 (実際のパッケージ名も変更を忘れないように！)
+paperPluginYaml {
+    name = "PaperTemplate" // TODO: プラグイン名を変更
+    author = "Namiu (うにたろう)" // TODO: 自分の名前に変更
+    website = "https://github.com/CraftersLife-Dev"
+    apiVersion = "1.21.10"
+
+    main = "$mainPackage.core.JavaPluginImpl"
+    bootstrapper = "$mainPackage.core.PluginBootstrapImpl"
+    loader = "$mainPackage.core.PluginLoaderImpl"
+
+    permissions {
+        register("${paperPluginYaml.name.get().lowercase()}.command.admin") {
+            description = "${paperPluginYaml.name}の管理者系コマンド"
+            default = Permission.Default.OP
+        }
+    }
+
+    dependencies {
+        bootstrap("MiniPlaceholders", Load.BEFORE, false)
+        server("LuckPerms", Load.BEFORE, false)
+    }
+}
+
+indraSpotlessLicenser {
+    licenseHeaderFile(rootProject.file("LICENSE_HEADER"))
+    property("name", paperPluginYaml.name)
+    property("author", paperPluginYaml.author)
+    property("contributors", paperPluginYaml.contributors)
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.runtimeDownload.get())
+    }
+}
+
+tasks {
+    compileJava {
+        options.compilerArgs.add("-parameters")
+    }
+
+    shadowJar {
+        archiveBaseName = paperPluginYaml.name
+        archiveClassifier = null as String?
+        gremlin {
+            listOf("xyz.jpenilla.gremlin")
+                .forEach {
+                    relocate(it, "$mainPackage.libs.$it")
+                }
+        }
+    }
+
+    runServer {
+        // runディレクトリの中にlog4j2.xmlを突っ込むとログの設定を変更可能
+        // Paper: https://github.com/PaperMC/Paper/blob/main/paper-server/src/main/resources/log4j2.xml
+        systemProperty("log4j.configurationFile", "log4j2.xml")
+        minecraftVersion("1.21.10")
+        downloadPlugins {
+            modrinth("luckperms", "v5.5.0-bukkit")
+            modrinth("miniplaceholders", "4zOT6txC")
+            hangar("PlaceholderAPI", "2.11.6")
+        }
+    }
+
+    writeDependencies {
+        repos.add("https://repo.papermc.io/repository/maven-public/")
+        repos.add("https://repo.maven.apache.org/maven2/")
+    }
+
+    checkstyle {
+        toolVersion = libs.versions.check.style.get()
+        configDirectory = rootDir.resolve(".checkstyle")
+    }
+}
